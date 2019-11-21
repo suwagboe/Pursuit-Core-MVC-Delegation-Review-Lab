b84ec9bc86bb6d3e580e828d2ab597bb90af2bd4
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-20 19:59:39 -0500

    " inside of DetailsViewController all of the items are linked to each other"

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/DetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsViewController.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by World Domination a Brand on 11/20/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var stepperLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
