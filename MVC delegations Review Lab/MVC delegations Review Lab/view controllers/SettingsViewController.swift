00e067a8dad279058d8289f0435b5062968ce702
uwagboe <sm.uwagboe@gmail.com>
2020-01-27 12:03:13 -0500

    put the elements inside of the view controller and attached them to the corresponding controllers

--- MVC delegations Review Lab/MVC delegations Review Lab/view controllers/SettingsViewController.swift
+++ MVC delegations Review Lab/MVC delegations Review Lab/view controllers/SettingsViewController.swift
@@ -14,12 +14,54 @@ class SettingsViewController: UITabBarController {
     @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var sampleSizeLabel: UILabel!
     @IBOutlet weak var numberOfFontSize: UILabel!
+    
+    @IBOutlet weak var sliderUpdateLabel: UILabel!
+    
+    
+    @IBOutlet weak var stepperUpdateLabel: UILabel!
+    
+    
+    var fontSize = 17
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureSettings()
+    }
+    
+    func configureSettings() {
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        stepper.minimumValue = 0
+        stepper.maximumValue = 30
+        
+        
+        slider.value = Float(fontSize)
+        slider.value = Float(fontSize)
+    }
+    
+    func valuesChanged() {
+        
+        
     }
     
     
-
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        
+        
+        
+    }
+    
+    
+    
 }
--- MVC delegations Review Lab/MVC delegations Review Lab/view controllers/mainViewController.swift
+++ MVC delegations Review Lab/MVC delegations Review Lab/view controllers/mainViewController.swift
@@ -12,24 +12,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    
-    var allMovies = Movie.allMovies
+     var allMovies = Movie.allMovies
 
+    
+    var fontSize: CGFloat = 17 {
+        didSet {
+            // when the size changes you want the tableView to change.
+            tableView.reloadData()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
-        
+        loadData()
     }
-
-
+    
+    
+    func loadData() {
+        var allMovies = Movie.allMovies
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingVC = segue.destination as? SettingsViewController else {
+            fatalError("The segue isnt working properly.")
+        }
+        //settingVC =
+    }
+    
+    
 }
 
+
 extension ViewController: UITableViewDataSource {
-    
+    // I know this is to populate the data.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return allMovies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -39,6 +61,8 @@ extension ViewController: UITableViewDataSource {
         cell.textLabel?.text = allMovies.first?.name
         cell.detailTextLabel?.text = allMovies.first?.genre
         
+        cell.textLabel?.font = UIFont(name: "times new roman", size: fontSize as! CGFloat)
+        
         // need to apply the indexPath. 
         
         return cell
@@ -47,7 +71,7 @@ extension ViewController: UITableViewDataSource {
 }
 
 extension ViewController: UITableViewDelegate {
-    
+    // what is the delegate for again?
     
     
 }
