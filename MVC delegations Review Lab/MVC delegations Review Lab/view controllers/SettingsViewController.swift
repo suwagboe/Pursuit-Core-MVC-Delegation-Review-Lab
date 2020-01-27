118b7175b3d45bbfda5c8a31f56db3ac4fc69a86
uwagboe <sm.uwagboe@gmail.com>
2020-01-27 18:18:56 -0500

    its done

--- MVC delegations Review Lab/MVC delegations Review Lab/view controllers/SettingsViewController.swift
+++ MVC delegations Review Lab/MVC delegations Review Lab/view controllers/SettingsViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class SettingsViewController: UITabBarController {
+// this is the delegate == and also a protocol
+protocol changingTheFontDelegate: AnyObject {
+    // where the action is happening... so this where the protocol goes.. can also be outside of this file.
+    
+    // class is a reference type.
+    // a protocol method.
+    // we call the protocol... which is the function
+    func changeFontSize(_ sender: SettingsViewController, font: CGFloat)
+    // this is looking for the font...
+    
+    
+    // object where the action is happen and also the object that is sending the info to the delegate
+    // this means to look at this place. // ask ches again if confused.
+    // https://useyourloaf.com/blog/quick-guide-to-swift-delegates/
+}
+
+class SettingsViewController: UIViewController {
+    
+    // making an instance of the protocol/delegate to gain access to the function/protocol inside of it.
+    weak var delegate: changingTheFontDelegate?
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var stepper: UIStepper!
@@ -21,45 +40,47 @@ class SettingsViewController: UITabBarController {
     @IBOutlet weak var stepperUpdateLabel: UILabel!
     
     
-    var fontSize = 17
-
+    var settingFontSize = 10.0{
+        didSet {
+            delegate?.changeFontSize(self, font: CGFloat(settingFontSize))
+            // this is to watch when the font size changes.
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSettings()
     }
     
     func configureSettings() {
+        // other slider configuration....
+        slider.minimumValue = 0.0
+        slider.maximumValue = 30.0
+        slider.value = 10.0
         
-        slider.minimumValue = 0
-        slider.maximumValue = 30
-        stepper.minimumValue = 0
-        stepper.maximumValue = 30
-        
-        
-        slider.value = Float(fontSize)
-        slider.value = Float(fontSize)
+        stepper.value = 10.0
+        stepper.minimumValue = 0.0
+        stepper.maximumValue = 30.0
+        stepper.stepValue = 1
     }
-    
-    func valuesChanged() {
-        
-        
-    }
-    
-    
-    
-    
+  
     @IBAction func sliderAction(_ sender: UISlider) {
+        settingFontSize = Double(sender.value)
+
+        slider?.value = sender.value
+        stepper?.value = Double(sender.value)
+        sampleSizeLabel.text = "The size is now\(sender.value)"
+           }
         
-        
-    }
-    
-    
-    
     
     @IBAction func stepperAction(_ sender: UIStepper) {
+        settingFontSize = Double(sender.value)
         
         
-        
+        sampleSizeLabel.text = "The size is now\(sender.value)"
+        stepper?.value = Double(sender.value)
+        slider?.value = Float(sender.value)
     }
     
     
--- MVC delegations Review Lab/MVC delegations Review Lab/view controllers/mainViewController.swift
+++ MVC delegations Review Lab/MVC delegations Review Lab/view controllers/mainViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
     @IBOutlet weak var tableView: UITableView!
     
-     var allMovies = Movie.allMovies
+     var TheMovies = Movie.allMovies
 
     
-    var fontSize: CGFloat = 17 {
+    var fontSize: CGFloat? = 10 {
+        // making optional to see if there is a value
         didSet {
             // when the size changes you want the tableView to change.
             tableView.reloadData()
@@ -26,52 +29,78 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        tableView.delegate = self
         tableView.dataSource = self
         loadData()
     }
     
     
-    func loadData() {
-        var allMovies = Movie.allMovies
+   private func loadData() {
+        TheMovies = Movie.allMovies
     }
     
+    // function to segue programatically.
+    private func settingSegue() {
+           guard let settingsController = storyboard?.instantiateViewController(identifier: "SettingsViewController") as? SettingsViewController else {
+               fatalError("it is not seguing properly..")
+           }
+        settingsController.settingFontSize = Double(fontSize!)
+        settingsController.delegate = self
+       
+       present(settingsController, animated: true)
+        
+        // you want to make a seperate reference to the protocol in order to access the info
+      
+        //why is the delegate here...
+        // delegate will get called... whenever you move between segues so even when you move from the second view to the first one this function will get called
+            // settingsController.delegate?.changeFontSize() = self
+        
+       }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let settingVC = segue.destination as? SettingsViewController else {
-            fatalError("The segue isnt working properly.")
-        }
-        //settingVC =
+    
+    @IBAction func settingButton(_ sender: UIBarButtonItem) {
+        
+        settingSegue()
     }
     
-    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let settingVC = segue.destination as? SettingsViewController else {
+//            fatalError("The segue isnt working properly.")
+//        }
+//       // settingVC.delegate = self
+//
+//        // didSet only listens...
+//        // delegate talks and listens to changes..
+//        // telling it to watch itself?? or watch whatever is called on the delegate of this functions
+//    }
 }
 
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, changingTheFontDelegate {
+    func changeFontSize(_ sender: SettingsViewController, font: CGFloat) {
+        
+        fontSize = CGFloat(font)
+    }
+    
     // I know this is to populate the data.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allMovies.count
+        return TheMovies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
         
-        cell.textLabel?.text = allMovies.first?.name
-        cell.detailTextLabel?.text = allMovies.first?.genre
+        let getaccessto = TheMovies[indexPath.row]
         
-        cell.textLabel?.font = UIFont(name: "times new roman", size: fontSize as! CGFloat)
+        cell.textLabel?.text = getaccessto.name
+        cell.detailTextLabel?.text = getaccessto.genre
         
-        // need to apply the indexPath. 
-        
+        //cell.textLabel?.font = UIFont(name: "Times New Roman", size: fontSize!)
+        cell.textLabel?.font = cell.textLabel?.font.withSize(fontSize!)
+        cell.detailTextLabel?.font = cell.detailTextLabel?.font.withSize(fontSize!)
         return cell
     }
     
 }
 
-extension ViewController: UITableViewDelegate {
-    // what is the delegate for again?
-    
-    
-}
+
